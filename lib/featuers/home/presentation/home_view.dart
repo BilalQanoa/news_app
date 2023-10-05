0f7d917d0b36587495448637202e345483de86f7
BilalQanoa <bilalqanoa@gmail.com>
2023-10-06 01:26:23 +0300

    create home view body class

--- lib/featuers/home/presentation/home_view.dart
+++ lib/featuers/home/presentation/home_view.dart
@@ -1,11 +1,16 @@
 
 import 'package:flutter/material.dart';
+import 'package:news_app/featuers/home/presentation/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
+
+
